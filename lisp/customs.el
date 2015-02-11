37972a7cb0789bc8efc82fd745ac1254b1215bf2
Rustem Muslimov <r.muslimov@gmail.com>
2015-02-11 10:23:50 -0800

    Fixes in flake8

--- lisp/customs.el
+++ lisp/customs.el
@@ -149,6 +149,11 @@
 
 (require 'browse-at-remote)
 
+(defun sort-lines-nocase ()
+  (interactive)
+  (let ((sort-fold-case t))
+    (call-interactively 'sort-lines)))
+
 ;; ends
 
 (provide 'customs)
--- lisp/mypython.el
+++ lisp/mypython.el
@@ -1,21 +1,27 @@
 
 
-(defun flycheck-virtualenv-set-python-executables ()
+(defun flycheck-local-flake8/get-venv-from-project-path (projectpath)
+  "Calc env path from project path"
+  (f-join (expand-file-name (getenv "WORKON_HOME"))
+          (car (last (split-string projectpath "/")))))
+
+(defun flycheck-local-flake8/flycheck-virtualenv-set-python-executables ()
   (let* ((venv (expand-file-name (vc-git-root (buffer-file-name))))
-         (envpath (s-replace "projects" "envs" venv)))
+         (envpath (flycheck-local-flake8/get-venv-from-project-path (substring venv 0 -1))))
     (if (file-exists-p envpath)
         (progn
           (pyvenv-activate envpath)
-          (setq flycheck-python-flake8-executable (executable-find "flake8")
-                flycheck-flake8rc (concat venv "setup.cfg")))
+          (setq-local flycheck-python-flake8-executable (executable-find "flake8"))
+          (setq-local flycheck-flake8rc (concat venv "setup.cfg")))
       (progn
         (pyvenv-deactivate)
         (exec-path-from-shell-initialize)
-        (setq flycheck-python-flake8-executable (executable-find "flake8")
-              flycheck-flake8rc (expand-file-name "~/.config/flake8")))
+        (setq-local flycheck-python-flake8-executable (executable-find "flake8"))
+        (setq-local flycheck-flake8rc (expand-file-name "~/.config/flake8"))
+        )
       )))
 
 (add-hook 'flycheck-before-syntax-check-hook
-          #'flycheck-virtualenv-set-python-executables 'local)
+          #'flycheck-local-flake8/flycheck-virtualenv-set-python-executables 'local)
 
 (provide 'mypython)
